a597c8ea4a5ed5ff4f666c9aea7a807e42f2b0ef
reynardh <reynardh@icloud.com>
2024-05-07 09:34:31 +0200

    added the video recorder and uploader component

--- src/modules/candidate/views/Candidate.vue
+++ src/modules/candidate/views/Candidate.vue
@@ -4,12 +4,9 @@
       <template #tab1>
         <div class="space-y-6 px-10">
           <div class="grid grid-cols-3 gap-4">
-            <Candidacy
-              v-for="candidacy in candidacies"
-              :key="candidacy.id"
+            <Candidacy v-for="candidacy in candidacies" :key="candidacy.id"
               :recruiter-name="candidacy.recruiter.firstName + ' ' + candidacy.recruiter.lastName"
-              :date="candidacy.createdAt"
-            />
+              :date="candidacy.createdAt" />
           </div>
         </div>
       </template>
@@ -17,30 +14,24 @@
       <template #tab2>
         <div class="space-y-6 px-10">
           <div class="flex gap-4">
-            <Button :outline="true" class="w-60">
+            <Button @click="showVideoRecorderModal = true" :outline="true" class="w-60">
               <div class="flex items-center">
                 <PhVideoCamera class="h-5 w-5 text-red-500" />
                 <span class="ml-2">Record Video</span>
               </div>
             </Button>
 
-            <Button :outline="true" class="w-60">
-              <div class="flex items-center">
-                <PhUpload class="h-5 w-5 text-red-500" />
-                <span class="ml-2">Upload Video</span>
-              </div>
-            </Button>
+            <VideoUploader />
           </div>
 
           <div class="grid grid-cols-3 gap-4">
-            <Video
-              v-for="video in videos"
-              :key="video.id"
-              :date="video.createdAt"
-              :is-live="video.isLive"
-            />
+            <Video v-for="video in videos" :key="video.id" :date="video.createdAt" :is-live="video.isLive" />
           </div>
-         
+
+          <Modal :show-modal="showVideoRecorderModal" :show-buttons="false" @close="showVideoRecorderModal = false" size="w-[35%]">
+            <VideoRecorder />
+          </Modal>
+
         </div>
       </template>
     </Tabs>
@@ -48,11 +39,17 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import Tabs from '@/components/Tabs.vue'
 import Candidacy from '../components/Candidacy.vue'
 import Video from '../components/Video.vue'
 import Button from '@/components/Button.vue'
+import Modal from '@/components/Modal.vue';
+import VideoRecorder from '../components/VideoRecorder.vue'
+import VideoUploader from '../components/VideoUploader.vue'
 import { PhVideoCamera, PhUpload } from '@phosphor-icons/vue'
+
+const showVideoRecorderModal = ref(false)
 
 const tabs = [
   { title: 'My candidacies', value: 'tab1' },
