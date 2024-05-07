4aae9d59f837c1770343b6b7f4d293abd183315d
reynardh <reynardh@icloud.com>
2024-05-07 11:18:40 +0200

    added the mux video player with dummy data

--- src/modules/candidate/components/Video.vue
+++ src/modules/candidate/components/Video.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="relative flex w-full flex-col gap-2 rounded-xl border border-slate-200 p-6">
-    <div class="aspect-square max-h-40 w-full shrink-0 rounded-xl bg-slate-200 p-2">
-      <PhImage class="h-full w-full text-slate-300" />
+    <div class="aspect-square max-h-40 w-full shrink-0 rounded-xl bg-slate-200">
+      <div :hidden="!isHiddenPlayer"  class="h-full w-full">        
+        <PhImage class="h-full w-full text-slate-300" />
+      </div>
+      <div :hidden="isHiddenPlayer">
+        <mux-player
+          class="aspect-square max-h-40 w-full shrink-0 rounded-xl bg-slate-200"
+          ref="muxplayer"
+          :playback-id="props.videoId"
+          metadata-video-title="Test video title"
+          metadata-viewer-user-id="user-id-007"
+        ></mux-player>
+      </div>
     </div>
 
     <div class="text-base font-medium">
@@ -26,11 +37,27 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref, computed } from 'vue';
 import { PhStar, PhImage, PhCheckCircle } from '@phosphor-icons/vue'
 import Button from '@/components/Button.vue'
+
+const muxplayer = ref<HTMLElement | null>(null)
+
+const isHiddenPlayer = computed(() => {
+  return props.videoId === '';
+})
+
+onMounted(() => {
+  if (document.getElementById('mux-player')) return; // was already loaded
+  var scriptTag = document.createElement("script");
+  scriptTag.src = "https://cdn.jsdelivr.net/npm/@mux/mux-player";
+  scriptTag.id = "mux-player";
+  document.getElementsByTagName('head')[0].appendChild(scriptTag);
+})
 
 const props = defineProps<{
   date: string
   isLive: boolean
+  videoId: string
 }>()
 </script>
