5f2b5491ade237b7481d1d52a0ba917472917a35
wamcandrew <138669629+wamcandrew@users.noreply.github.com>
2024-05-09 01:06:56 +0200

    login and logout

--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -27,7 +27,7 @@
           ]"
           @click="goToLogin"
         >
-          <PhUserCircle class="mr-2 h-5 w-5 text-primary-500" />
+          <PhSignIn class="mr-2 h-5 w-5 text-primary-500" />
           Login
         </button>
       </template>
@@ -36,7 +36,7 @@
       //////////////////////////
       */
 
-      <template #sign-out="{ active }">
+      <template #logout="{ active }">
         <button
           :class="[
             active ? 'bg-slate-100' : 'text-gray-900',
@@ -44,7 +44,7 @@
           ]"
         >
           <PhSignOut class="mr-2 h-5 w-5 text-primary-500" />
-          Sign out
+          Logout
         </button>
       </template>
     </NavbarDropdown>
@@ -55,7 +55,7 @@
 import Navbar from '@/components/navbar/Navbar.vue'
 import NavbarDropdown from '@/components/navbar/NavbarDropdown.vue'
 
-import { PhUserCircle, PhSignOut } from '@phosphor-icons/vue'
+import { PhUserCircle, PhSignOut, PhSignIn } from '@phosphor-icons/vue'
 import { useRouter } from 'vue-router'
 import { useAuth0 } from '@auth0/auth0-vue';
 const auth0 = useAuth0();
@@ -69,7 +69,7 @@ const navbarItems = [
   { title: 'Contact', to: 'home' }
 ]
 
-const navbarDropdownItems = [{ name: 'profile' }, { name: 'login' }, { name: 'sign-out' }]
+const navbarDropdownItems = [{ name: 'profile' }, { name: 'login' }, { name: 'logout' }]
 
 function goToProfile() {
   router.push({
@@ -79,6 +79,14 @@ function goToProfile() {
 
 function goToLogin() {
   auth0.loginWithRedirect();
+}
+
+function goToLogout() {
+  auth0.logout({ 
+    logoutParams: { 
+      returnTo: window.location.origin 
+     } 
+  });
 }
   
 </script>
