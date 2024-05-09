17fb107f7af2f239137950e0bded2506074416d2
reynardh <reynardh@icloud.com>
2024-05-09 09:46:55 +0200

    addd slider and select component

---
+++ src/components/Slider.vue
@@ -0,0 +1,20 @@
+<template>
+    <SliderRoot
+      v-model="sliderValue" class="relative flex items-center select-none touch-none w-[180px] h-5" :max="100"
+      :step="10"
+    >
+      <SliderTrack class="bg-red-500 relative grow rounded-full h-[3px]">
+        <SliderRange class="absolute bg-red-500 rounded-full h-full" />
+      </SliderTrack>
+      <SliderThumb
+        class="block w-5 h-5 bg-red-500 shadow-[0_2px_2px] shadow-red-500 rounded-[10px] hover:bg-red-500 focus:outline-none"
+        aria-label="Volume"
+      />
+    </SliderRoot>
+  </template>
+
+<script setup lang="ts">
+import { SliderRange, SliderRoot, SliderThumb, SliderTrack } from 'radix-vue'
+
+const sliderValue = defineModel<number[]>('value')
+</script>
--- src/modules/recruiter/views/Recruiter.vue
+++ src/modules/recruiter/views/Recruiter.vue
@@ -4,18 +4,27 @@
       <template #tab1>
         <div class="flex px-10">
           <div class="w-full max-w-60 space-y-3">
-            <div class="mb-4 font-medium">Job Title</div>
-            <Checkbox v-model:checked="jobTitle.web" label="Web" />
-            <Checkbox v-model:checked="jobTitle.mobile" label="Mobile" />
+            <div class="font-medium">Job Title</div>
+            <Select v-model:selected:value="selectedJobTitle" :options="['web', 'mobile', 'all']" />
+            <!-- <Checkbox v-model:checked="jobTitle.web" label="Web" />
+            <Checkbox v-model:checked="jobTitle.mobile" label="Mobile" /> -->
             
             <div class="mb-4 pt-6 font-medium">Contract Type</div>
             <Checkbox v-model:checked="contractType.apprenti" label="Apprenti" />
             <Checkbox v-model:checked="contractType.employee" label="Employee" />
             <Checkbox v-model:checked="contractType.cadre" label="Cadre" />
+
+            <div class="mb-4 pt-6 font-medium">Seeking Rate</div>
+            <Checkbox v-model:checked="enableSeekingRate" label="Enable Seeking Rate" />
+            <div class="flex items-center gap-x-2">
+              <Slider v-model:value="seekingRate" />
+              <span>{{ seekingRate[0] }} %</span>
+            </div>
           </div>
 
-          <div :hidden="candidates.length == 0" class="grid grid-cols-1 gap-4">
+          <div v-if="!isLoading && candidates.length > 0"  class="grid grid-cols-1 gap-4">
             <Candidate
+              :is-loading="true"
               v-for="candidate in candidates"
               :key="candidate.id"
               :candidateId="candidate.id"
@@ -23,6 +32,12 @@
               :lastName="candidate.last_name"
               :bio="candidate.bio"
             />
+          </div>
+          <div v-if="!isLoading && candidates.length == 0" class="m-auto font-medium text-xl text-gray-700">
+            Not found
+          </div>
+          <div class="m-auto" v-if="isLoading">
+            <Spinner />
           </div>
         </div>
       </template>
@@ -63,16 +78,19 @@ import { ref, onMounted, watchEffect, reactive } from 'vue'
 import Tabs from '@/components/Tabs.vue'
 import Candidate from '../components/Candidate.vue'
 import Checkbox from '@/components/Checkbox.vue'
+import Select from '@/components/Select.vue'
+import Slider from '@/components/Slider.vue'
 import API from '@/utils/api/api';
+import Spinner from '@/components/Spinner.vue'
 import type { ICandidateFilterObj } from '@/utils/common/types'
-import { SEEKING_FIELD, SEEKING_CONTRACT_TYPE } from '@/utils/common/enums';
+import { SEEKING_CONTRACT_TYPE } from '@/utils/common/enums';
 
 const candidates = ref<any[]>([])
 
-const jobTitle = reactive({
-  web: false,
-  mobile: false
-})
+const selectedJobTitle = ref<string>("");
+const seekingRate = ref<number[]>([20]);
+const enableSeekingRate = ref<boolean>(false);
+const isLoading = ref<boolean>(true);
 
 const contractType = reactive({
   apprenti: false,
@@ -86,24 +104,38 @@ const tabs = [
 ]
 
 onMounted(() => {
+  isLoading.value = true;
   API.getCandidates()
-    .then(response => {
-      candidates.value = response.data
-    })
+  .then(response => {
+    candidates.value = response.data
+    isLoading.value = false;
+  })
 })
 
 watchEffect(() => {
+  isLoading.value = true;
   let queryParams: ICandidateFilterObj = {
     seeking_field: [],
     seeking_rate: [],
     seeking_contract_type: []
   };
-  if (jobTitle.web) {
-    queryParams.seeking_field.push(SEEKING_FIELD.WEB);
+  
+  if(selectedJobTitle.value) {
+    queryParams.seeking_field.push(selectedJobTitle.value)
+    if(selectedJobTitle.value == 'all') {
+      queryParams.seeking_field  = []
+    }
   }
-  if (jobTitle.mobile) {
-    queryParams.seeking_field.push(SEEKING_FIELD.MOBILE);
+
+  if(seekingRate.value && enableSeekingRate.value) {
+    queryParams.seeking_rate.push(seekingRate.value[0].toString())
   }
+  // if (jobTitle.web) {
+  //   queryParams.seeking_field.push(SEEKING_FIELD.WEB);
+  // }
+  // if (jobTitle.mobile) {
+  //   queryParams.seeking_field.push(SEEKING_FIELD.MOBILE);
+  // }
   if (contractType.apprenti) {
     queryParams.seeking_contract_type.push(SEEKING_CONTRACT_TYPE.APPRENTI);
   }
@@ -117,6 +149,7 @@ watchEffect(() => {
   API.getCandidates(queryParams)
     .then(response => {
       candidates.value = response.data
+      isLoading.value = false;
     })
 })
 
